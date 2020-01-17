56d47638af0e507bf0c7d822bef2a24b0a773c3b
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-01-17 18:52:50 +0300

    Added layer relations. The dbContext has been moved to the DAL.

--- AspNetCoreProject/AspNetCoreProject.DAL/ApplicationDbContext.cs
+++ AspNetCoreProject/AspNetCoreProject.DAL/ApplicationDbContext.cs
@@ -4,13 +4,21 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace AspNetCoreProject.Web.Data
+namespace AspNetCoreProject.DAL
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        private static bool CreateNewDataBase = true;
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            if (CreateNewDataBase)
+            {
+                Database.EnsureDeleted();
+                Database.EnsureCreated();
+                CreateNewDataBase = false;
+            }
         }
     }
 }
